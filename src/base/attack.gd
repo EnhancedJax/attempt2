019bbfc0f2f7a87c155477b0f55755c068133066
Jax <enhancedjax@gmail.com>
2025-02-23 18:39:47 +0800

    save: refactor file

--- src/base/attack.gd
+++
@@ -1,7 +0,0 @@
-class_name Attack extends Resource
-
-var damage: int = 0
-var recoil: float = 0  # Force applied to the shooter
-var knockback: float = 0  # Force applied to the target
-var direction: Vector2 = Vector2.ZERO  # Direction of the attack
-var source: Node = null  # Who fired the attack
--- src/enemies/dummy.gd
+++
@@ -1,4 +0,0 @@
-class_name DummyCharacter extends GunUser
-
-func _ready() -> void:
-    equip_weapon([BasicGun, preload("res://weapons/rifle.tres")])
